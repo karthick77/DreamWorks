bab73e89acaa102ccb9ae7b8c79d4027bdab2441
karthick77 <mail2bkarthick@gmail.com>
2023-06-04 14:38:50 +0530

    dev updated

--- DreamWorks/src/FirstPack/First.java
+++ DreamWorks/src/FirstPack/First.java
@@ -10,13 +10,13 @@ public class First {
 	
    public static void doSomething() {
 		
-		System.out.println("Please First Do Something");
+		System.out.println("Please First Do Something and Sleep");
 
 	}
    
    public static void printSomething() {
 		
-		System.out.println("Please First Print Something");
+		System.out.println("Please First Print Something and Read");
 
 	}
 	
