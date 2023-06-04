2bb0d56dad2b3defe72ae74efef29ec0e6a84966
karthick77 <mail2bkarthick@gmail.com>
2023-06-04 13:53:45 +0530

    initial commit

---
+++ DreamWorks/src/FirstPack/First.java
@@ -0,0 +1,18 @@
+package FirstPack;
+
+public class First {
+
+	public static void main(String[] args) {
+		
+		System.out.println("First Class");
+
+	}
+	
+   public static void doSomething() {
+		
+		System.out.println("First Do Something");
+
+	}
+	
+
+}
